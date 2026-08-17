44113472dfe66063fd163d7a965f8a3072ffbac7
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-17 23:15:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassWork1
+{
+    internal class Mobile // It is a Class 
+    {
+        int Price;
+        string name;
+        string Color;
+
+        public void calling()
+        {
+            Console.WriteLine("Voice msg");
+        }
+        public void Chatting()
+        {
+            Console.WriteLine("Chat");
+        }
+        public void VideoCalling()
+        {
+            Console.WriteLine("VideoCalling");
+        }
+        static void Main(string[] args)
+        {
+            Mobile m = new Mobile();
+            m.Price = 75000;
+            m.name = "Nothing";
+            m.Color = "White";
+
+            Console.WriteLine(m.Price);
+            Console.WriteLine(m.name);
+            Console.WriteLine(m.Color);
+
+            m.calling(); m.Chatting(); m.VideoCalling();
+        }
+    }
+}

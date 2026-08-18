805fab658f18e0dd2e492b2bd3acd32a1da81b29
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-18 21:46:10 +0530

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOps
+{
+    internal class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            Bus myBus = new Bus();
+
+            myBus.Color = "Blue";
+
+            Console.WriteLine("Bus Color: " + myBus.Color);
+        }
+    }
+
+    class Bus
+    {
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+}
